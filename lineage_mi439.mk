96a9d33721086eb06590bd95e7fe2e011bf5225a
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-01-02 17:17:38 +0800

    mi439: Rebrand

--- lineage_mi439.mk
+++ lineage_mi439.mk
@@ -12,22 +12,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from olives device
-$(call inherit-product, device/xiaomi/olives/device.mk)
+# Inherit from mi439 device
+$(call inherit-product, device/xiaomi/mi439/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := olives
-PRODUCT_NAME := lineage_olives
+PRODUCT_DEVICE := mi439
+PRODUCT_NAME := lineage_mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 8 / 8A / 8A Dual
+PRODUCT_MODEL := SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNCNXM release-keys"
+    PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNCNXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
